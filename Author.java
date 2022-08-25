68fe96acad644978e6118c3b3833e9f8f04f3586
northinrtm <artsdurden@gmail.com>
2022-08-25 17:24:49 +0300

    первый коммит

---
+++ Author.java
@@ -0,0 +1,8 @@
+public class Author {
+    Author(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    String firstName;
+    String lastName;
+}
